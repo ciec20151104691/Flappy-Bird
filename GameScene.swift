d4a0da5dd5801a50eb308b2cf1ec3f18d22f9bd5
ciec20151104691 <s20151104691@admindeiMac-88.local>
2018-05-11 15:22:10 +0800

    设置了背景与前景两部分，并找好图片资源，具体方法还没写

--- GameScene.swift
+++ GameScene.swift
@@ -8,14 +8,34 @@
 
 import SpriteKit
 
-    override func didMoveToView(view: SKView) {
-        
-    }
+import SpriteKit
 
+enum picture: CGFloat {
+    case background
+    case prospects
+    case role
+}
+
+
+class GameScene: SKScene {
+    
+    let world = SKNode()
+    var startpoint: CGFloat = 0
+    var worldheight: CGFloat = 0
+    
+    
+    override func didMoveToView(view: SKView) {
+        addChild(world)
+        makebackground()
+        makeprospects()
+    }
+    
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
-
+        
     }
 }
+
