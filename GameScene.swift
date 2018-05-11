0ec1d36326b1c543d1e0951b587bb73a3fd50827
ciec20151104691 <s20151104691@admindeiMac-88.local>
2018-05-11 15:10:58 +0800

    创建了背景，还未完善

---
+++ GameScene.swift
@@ -0,0 +1,21 @@
+//
+//  GameScene.swift
+//  game
+//
+//  Created by s20151104691 on 18/5/9.
+//  Copyright © 2018年 ltx. All rights reserved.
+//
+
+import SpriteKit
+
+    override func didMoveToView(view: SKView) {
+        
+    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+
+    }
+}
---
+++ GameViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GameViewController.swift
+//  game
+//
+//  Created by s20151104691 on 18/5/9.
+//  Copyright © 2018年 ltx. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let skview = self.view as? SKView {
+            if skview.scene == nil {
+                //  创建场景
+                let lw = skview.bounds.size.height / skview.bounds.size.width
+                let screen = GameScene(size:CGSize(width: 320, height: 320 * lw))
+                skview.showsFPS = true
+                skview.showsNodeCount = true
+                skview.showsPhysics = true
+                skview.ignoresSiblingOrder = true
+                
+                screen.scaleMode = .AspectFill
+                
+                skview.presentScene(screen)
+            }
+        }
+        
+        
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
